3cb1de56a5f36252313e3e2553e35b149d25be94
abraham <code.arb2712@gmail.com>
2025-07-25 10:36:34 -0400

    fix design

--- app/components/ui/Header/Header.tsx
+++ app/components/ui/Header/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
     <header className="container mx-auto max-w-[1200px] px-2 flex justify-between py-6">
       <figure className="flex items-center gap-1">
         <p className="font-[900] text-xl">{"</>"}</p>
-        <p className="font-[900] text-xl">Abraham Develop.</p>
+        <p className="font-[900] text-xl">Abraham Developer.</p>
       </figure>
 
       <Navbar />
